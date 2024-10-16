0962d27b85d62eb20a819046c8033264676b8b32
Ranveer Dhir <63573796+ranveerdhir@users.noreply.github.com>
2024-10-16 10:06:22 -0700

    Add files via upload
    Add JavaScript to fetch and display country data using REST API

---
+++ app.js
@@ -0,0 +1,81 @@
+const dropDown = document.querySelector('.dropdownMenu');
+const dropOptions = document.querySelector('.drop-options');
+const toggle = document.querySelector('.toggle');
+const icon = document.querySelector('.bx');
+const countries = document.querySelector('.countries');
+const search = document.querySelector('.search');
+const regions = document.querySelector('.regions');
+
+
+toggle.addEventListener('click', e=> {
+    document.body.classList.toggle('dark-mode');
+    toggle.classList.toggle('dark-mode');
+    dropDown.classList.toggle('dark-mode' )
+    
+
+})
+
+dropDown.addEventListener('click',e =>{
+    
+    dropOptions.classList.toggle('show-options');
+})
+
+async function getCountries() {
+    const URL = await fetch('https://restcountries.com/v3.1/all');
+    const res = await URL.json();
+    console.log(res);
+    res.forEach(api =>{
+        showCountry(api);
+    });
+}
+getCountries();
+
+function showCountry(data) {
+    const country = document.createElement('div');
+    country.classList.add('country');
+    country.innerHTML = 
+    `<div class="country-img"> 
+       <img src =${data.flags.png} alt="">
+     </div>
+    
+    <div class="country-details">
+       <h5 class="countryName">${data.name.common}</h5> 
+       <p><strong>population:</strong>${data.population}</p>
+       <p class = "regionName"><strong>Region</strong>${data.region}</p>
+       <p><strong>Capital</strong>${data.capital}</p>
+    </div>`
+    countries.appendChild(country) 
+
+}
+const countryName = document.getElementsByClassName('countryName');
+search.addEventListener('input', e => {
+     Array.from(countryName).forEach(country => {
+        if(country.innerText.toLowerCase().includes(search.value.toLowerCase())) {
+            country.parentElement.parentElement.style.display = "grid";
+        }else {
+            country.parentElement.parentElement.style.display = "none";
+        }
+     })
+})
+
+const regionName = document.getElementsByClassName('.regionName');
+
+regions.forEach(region => {
+    region.addEventListener('click',e => {
+        console.log("hi");
+        Array.from(regionName).forEach(element => {
+            if(element.innerText.includes(region.innerText) || region.innerText == "ALL"){
+                element.parentElement.parentElement.style.display = "grid";
+
+            }else {
+                element.parentElement.parentElement.style.display = "none";
+            }
+            console.log("done");
+        }) 
+    })
+} )
+    
+    
+
+
+    
